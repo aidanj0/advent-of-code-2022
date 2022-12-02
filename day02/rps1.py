5e5d05e5ac07da4d23828d70fb4a9fe2268459b0
Aidan <aidanjohansson01@gmail.com>
2022-12-02 00:27:32 -0500

    day 2!

---
+++ day02/rps1.py
@@ -0,0 +1,14 @@
+with open('./day02/rps.txt') as f:
+    score = 0
+    for line in f:
+        s = line.strip()
+        spl = s.split(' ')
+        opp = spl[0]
+        me = spl[1]
+        if opp == 'A':
+            score += {'X' : 4, 'Y' : 8, 'Z' : 3}[me]
+        elif opp == 'B':
+            score += {'X' : 1, 'Y' : 5, 'Z' : 9}[me]
+        else:
+            score += {'X' : 7, 'Y' : 2, 'Z' : 6}[me]
+    print(score)
---
+++ day02/rps2.py
@@ -0,0 +1,14 @@
+with open('./day02/rps.txt') as f:
+    score = 0
+    for line in f:
+        s = line.strip()
+        spl = s.split(' ')
+        opp = spl[0]
+        me = spl[1]
+        if opp == 'A':
+            score += {'X' : 3, 'Y' : 4, 'Z' : 8}[me]
+        elif opp == 'B':
+            score += {'X' : 1, 'Y' : 5, 'Z' : 9}[me]
+        else:
+            score += {'X' : 2, 'Y' : 6, 'Z' : 7}[me]
+    print(score)
