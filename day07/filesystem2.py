b3f86fff95b49ed46c63bc8ffed3af00f99fc72f
Aidan <aidanjohansson01@gmail.com>
2022-12-07 13:59:12 -0500

    day 7!

---
+++ day07/filesystem2.py
@@ -0,0 +1,51 @@
+def pathify(path):
+    res = ''
+    for node in path:
+        res += (node + ' ')
+    return res[:-1]
+
+edges = {}
+base_values = {}
+
+with open('./day07/filesystem.txt') as f:
+    lines = []
+    for line in f:
+        s = line.strip()
+        lines.append(s)
+
+    lines.pop(0)
+    path = ['/']
+    while lines:
+        line = lines.pop(0)
+        if line == '$ ls':
+            while lines and lines[0][0] != '$':
+                l = lines.pop(0)
+                if l[0] == 'd':
+                    edges[pathify(path)] = edges.setdefault(pathify(path), []) + [pathify(path) + ' ' + l[4:]]
+                    edges[pathify(path) + ' ' + l[4:]] = []
+                else:
+                    base_values[pathify(path)] = base_values.setdefault(pathify(path), 0) + int(l.split(' ')[0])
+
+        elif line == '$ cd ..':
+            path.pop()
+
+        elif '$ cd ' in line:
+            path.append(line[5:])
+
+sums = []
+outerspace = 0
+for start in edges.keys():
+    frontier = [start]
+    nsum = 0
+    while frontier:
+        node = frontier.pop()
+        if node in base_values : nsum += base_values[node]
+        if node in edges : frontier += edges[node]
+    sums.append(nsum)
+    if start == '/' : outerspace = nsum
+target = 30000000 - (70000000 - outerspace)
+ssums = sorted(sums)
+for s in ssums:
+    if s >= target:
+        print(s)
+        break
