fb3880ee3186c9e912d8d3c0a7746ff7a2df5895
Aidan <aidanjohansson01@gmail.com>
2022-12-18 21:30:26 -0600

    partial day 18

---
+++ day18/obsidian1.py
@@ -0,0 +1,17 @@
+import copy
+
+posns = set()
+with open('./day18/test.txt') as f:
+    for line in f:
+        s = line.strip()
+        posns.add( tuple( [ int(c) for c in s.split(',') ] ) )
+res = 0
+for posn in posns:
+    for i in range(3):
+        for d in [-1, 1]:
+            sample = list(posn)
+            sample[i] += d
+            if tuple(sample) not in posns:
+                res += 1
+
+print(res)
